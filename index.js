6bf268318f474d53df3e0755298abb2645eadf7b
MoonaAijaz <moona.aijaz91@gmail.com>
2024-05-25 16:21:05 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const age = 24;
 const name = 'Moona Aijaz'
 const dob = '27/08/1991'
+const define([
+    'require',
+    'dependency'
+], function(require, factory) {
+    'use strict';
+    
+});
 const year = '1998'
 close
